f094d05bc3359d5c2dbf3554d786581e978d1a23
vitorregisr <vitorregisrr@gmail.com>
2020-07-07 12:27:49 -0300

    QA Fixies

--- src/containers/hipica/Home/index.js
+++ src/containers/hipica/Home/index.js
@@ -118,28 +118,28 @@ const Home = (props) => {
             <nav className="Home__menu">
                 <div className="Home__menu-list" data-show={isToggled}>
                     <Link to="/hipica/historia" className="Home__menu-item">
-                        <span className="title">{data.pages[1].titulo}</span>
-                        <span className="desc">{data.pages[1].phrase}</span>
+                        <span className="title">{data.pages[7].titulo}</span>
+                        <span className="desc">{data.pages[7].phrase}</span>
                     </Link>
                     <Link to="/hipica/equipe" className="Home__menu-item">
-                        <span className="title">{data.pages[2].titulo}</span>
-                        <span className="desc">{data.pages[2].phrase}</span>
+                        <span className="title">{data.pages[8].titulo}</span>
+                        <span className="desc">{data.pages[8].phrase}</span>
                     </Link>
                     <Link to="/hipica/cavalos" className="Home__menu-item">
-                        <span className="title">{data.pages[3].titulo}</span>
-                        <span className="desc">{data.pages[3].phrase}</span>
+                        <span className="title">{data.pages[9].titulo}</span>
+                        <span className="desc">{data.pages[9].phrase}</span>
                     </Link>
                     <Link to="/hipica/resultados" className="Home__menu-item">
-                        <span className="title">{data.pages[4].titulo}</span>
-                        <span className="desc">{data.pages[4].phrase}</span>
+                        <span className="title">{data.pages[10].titulo}</span>
+                        <span className="desc">{data.pages[10].phrase}</span>
                     </Link>
                     <Link to="/hipica/eventos" className="Home__menu-item">
-                        <span className="title">{data.pages[5].titulo}</span>
-                        <span className="desc">{data.pages[5].phrase}</span>
+                        <span className="title">{data.pages[11].titulo}</span>
+                        <span className="desc">{data.pages[11].phrase}</span>
                     </Link>
                     <Link to="/hipica/midia" className="Home__menu-item">
-                        <span className="title">{data.pages[6].titulo}</span>
-                        <span className="desc">{data.pages[6].phrase}</span>
+                        <span className="title">{data.pages[12].titulo}</span>
+                        <span className="desc">{data.pages[12].phrase}</span>
                     </Link>
                 </div>
             </nav>
